5a10b3b5bfb7f62bc45e2c38fa274d88f3c5bab4
iwayamak <iwayamak@matsubabreak.com>
2020-03-12 19:11:36 +0900

    fix s3_list logging format

--- jetline/module/sub_module/s3/s3_list.py
+++ jetline/module/sub_module/s3/s3_list.py
@@ -2,6 +2,7 @@
 
 import logging
 import fnmatch
+import dateutil.tz as tz
 from ..abc.sub_module import SubModule
 from ...sub_module_parameter.s3.s3_list_parameter import S3ListParameter
 from ....command.s3.s3_list_command import S3ListCommand
@@ -30,4 +31,9 @@ class S3List(SubModule):
             [s3_object for s3_object in object_list
              if fnmatch.fnmatch(s3_object.key, self._parameter.s3_file_path.get())]
         for result_s3_file in self._result_s3_file_list:
-            logger.info(result_s3_file.key)
+            last_modified = \
+                result_s3_file.last_modified.astimezone(tz.tzlocal()).strftime('%Y-%m-%d %H:%M:%S')
+            size = str(result_s3_file.size).rjust(13)
+            logger.info(
+                f'{last_modified} {size} Bytes {result_s3_file.key}'
+            )
